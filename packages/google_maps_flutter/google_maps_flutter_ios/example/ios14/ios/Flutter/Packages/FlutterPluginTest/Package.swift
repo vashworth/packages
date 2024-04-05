861d9a5382c425886609db8fa7e77f8eda17262c
Victoria Ashworth <vashworth@google.com>
2024-04-05 14:53:27 -0500

    update google_maps_flutter to new model

--- packages/google_maps_flutter/google_maps_flutter_ios/example/ios14/ios/Flutter/Packages/FlutterPluginTest/Package.swift
+++ packages/google_maps_flutter/google_maps_flutter_ios/example/ios14/ios/Flutter/Packages/FlutterPluginTest/Package.swift
@@ -9,14 +9,12 @@ let package = Package(
         .iOS("14.0"),
     ],
     dependencies: [
-        .package(path: "../ephemeral/FlutterGeneratedPluginSwiftPackage"),
-        .package(name: "google_maps_flutter_ios", path: "../ephemeral/.symlinks/plugins/google_maps_flutter_ios/ios/google_maps_flutter_ios")
+        .package(name: "google_maps_flutter_ios", path: "../../../../../../ios/google_maps_flutter_ios")
     ],
     targets: [
         .testTarget(
             name: "FlutterPluginTest",
             dependencies: [
-                "FlutterGeneratedPluginSwiftPackage",
                 .product(name: "google_maps_flutter_ios_test", package: "google_maps_flutter_ios")
             ]
         )
--- packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Package.swift
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Package.swift
@@ -3,21 +3,17 @@
 
 import PackageDescription
 
-let pluginMinimumIOSVersion = Version("14.0.0")
-let pluginMinimumMacOSVersion = Version("10.14.0")
-
 let package = Package(
     name: "google_maps_flutter_ios",
     platforms: [
-        flutterMinimumIOSVersion(pluginTargetVersion: pluginMinimumIOSVersion),
-        flutterMinimumMacOSVersion(pluginTargetVersion: pluginMinimumMacOSVersion)
+        .iOS("14.0"),
+        .macOS("10.14")
     ],
     products: [
         .library(name: "google_maps_flutter_ios", type: .static, targets: ["google_maps_flutter_ios"]),
         .library(name: "google_maps_flutter_ios_test", targets: ["google_maps_flutter_ios_test"])
     ],
     dependencies: [
-        flutterFrameworkDependency(),
         .package(url: "https://github.com/googlemaps/ios-maps-sdk", "8.3.1"..<"9.0.0"),
         .package(url: "https://github.com/erikdoe/ocmock", revision: "ef21a2ece3ee092f8ed175417718bdd9b8eb7c9a") // v3.9.1
     ],
@@ -25,7 +21,6 @@ let package = Package(
         .target(
             name: "google_maps_flutter_ios",
             dependencies: [
-                .product(name: "Flutter", package: "Flutter"),
                 .product(name: "GoogleMaps", package: "ios-maps-sdk"),
                 .product(name: "GoogleMapsBase", package: "ios-maps-sdk"),
                 .product(name: "GoogleMapsCore", package: "ios-maps-sdk")
@@ -50,53 +45,3 @@ let package = Package(
         )
     ]
 )
-
-
-/// Returns the Package.Dependency for the Flutter framework.
-///
-/// Do not edit or remove. Used by the Flutter CLI to ensure the correct framework is used.
-///
-/// - Parameters:
-///   - localFrameworkPath: The path to the Flutter framework Swift Package. Can be used when
-///     locally developing the package. Will not be used when ran with the Flutter CLI.
-/// - Returns: A Package.Dependency for the Flutter framework.
-func flutterFrameworkDependency(localFrameworkPath: String? = nil) -> Package.Dependency {
-   let flutterFrameworkPackagePath = localFrameworkPath ?? "unknown"
-   return .package(name: "Flutter", path: flutterFrameworkPackagePath)
-}
-
-/// Returns the SupportedPlatform for iOS, ensuring the minimum deployment target version for the
-/// iOS platform is always greater than or equal to that of the Flutter framework.
-///
-/// Do not edit or remove. Used by the Flutter CLI to ensure the correct minimum deployment target
-/// version for iOS is used.
-///
-/// - Parameters:
-///   - pluginTargetVersion: The minimum deployment target version for iOS.
-/// - Returns: The SupportedPlatform for iOS.
-func flutterMinimumIOSVersion(pluginTargetVersion: Version) -> SupportedPlatform {
-   let iosFlutterMinimumVersion = Version("12.0.0")
-   var versionString = pluginTargetVersion.description
-   if iosFlutterMinimumVersion > pluginTargetVersion {
-       versionString = iosFlutterMinimumVersion.description
-   }
-   return SupportedPlatform.iOS(versionString)
-}
-
-/// Returns the SupportedPlatform for macOS, ensuring the minimum deployment target version for the
-/// macOS platform is always greater than or equal to that of the Flutter framework.
-///
-/// Do not edit or remove. Used by the Flutter CLI to ensure the correct minimum deployment target
-/// version for macOS is used.
-///
-/// - Parameters:
-///   - pluginTargetVersion: The minimum deployment target version for macOS.
-/// - Returns: The SupportedPlatform for macOS.
-func flutterMinimumMacOSVersion(pluginTargetVersion: Version) -> SupportedPlatform {
-   let macosFlutterMinimumVersion = Version("10.14.0")
-   var versionString = pluginTargetVersion.description
-   if macosFlutterMinimumVersion > pluginTargetVersion {
-       versionString = macosFlutterMinimumVersion.description
-   }
-   return SupportedPlatform.macOS(versionString)
-}
