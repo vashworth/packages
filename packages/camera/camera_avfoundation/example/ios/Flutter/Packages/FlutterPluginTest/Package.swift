feca6c5836c3c152a4ca7ac643194c4a7471c455
Victoria Ashworth <vashworth@google.com>
2024-04-05 14:28:46 -0500

    update camera_avfoundation to new model

--- packages/camera/camera_avfoundation/example/ios/Flutter/Packages/FlutterPluginTest/Package.swift
+++ packages/camera/camera_avfoundation/example/ios/Flutter/Packages/FlutterPluginTest/Package.swift
@@ -9,14 +9,12 @@ let package = Package(
         .iOS("12.0"),
     ],
     dependencies: [
-        .package(path: "../ephemeral/FlutterGeneratedPluginSwiftPackage"),
-        .package(name: "camera_avfoundation", path: "../ephemeral/.symlinks/plugins/camera_avfoundation/ios/camera_avfoundation")
+        .package(name: "camera_avfoundation", path: "../../../../../ios/camera_avfoundation")
     ],
     targets: [
         .testTarget(
             name: "FlutterPluginTest",
             dependencies: [
-                "FlutterGeneratedPluginSwiftPackage",
                 .product(name: "camera_avfoundation_test", package: "camera_avfoundation")
             ]
         )
