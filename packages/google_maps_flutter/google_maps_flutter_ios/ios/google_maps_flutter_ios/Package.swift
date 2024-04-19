00217658e36b642943f3b97c0ff9a141306545f2
Victoria Ashworth <vashworth@google.com>
2024-04-19 16:09:15 -0500

    update range for ios-maps-sdk

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Package.swift
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Package.swift
@@ -13,7 +13,13 @@ let package = Package(
         .library(name: "google-maps-flutter-ios", type: .static, targets: ["google_maps_flutter_ios"])
     ],
     dependencies: [
-        .package(url: "https://github.com/googlemaps/ios-maps-sdk", "8.3.1"..<"9.0.0")
+        //  Allow any version up to the next breaking change after the latest version that
+        // has been confirmed to be compatible via an example in examples/. See discussion
+        // in https://github.com/flutter/flutter/issues/86820 for why this should be as
+        // broad as possible.
+        // Versions earlier than 8.4 can't be supported because that's the first version
+        // that supports privacy manifests.
+        .package(url: "https://github.com/googlemaps/ios-maps-sdk", "8.4.0"..<"9.0.0")
     ],
     targets: [
         .target(
